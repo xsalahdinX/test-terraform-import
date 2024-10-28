38b5540a9654347914d0abcdabe213d4fd38e808
Salahdin_Hafez <102380926+xsalahdinX@users.noreply.github.com>
2024-10-28 14:38:35 +0300

    Update import.tf

--- import.tf
+++ import.tf
@@ -1,7 +1,7 @@
 import {
  # ID of the cloud resource
  # Check provider documentation for importable resources and format
- id = "import-bucket-tf15"
+ id = "salahdin-ttestt"
  # Resource address
  to = aws_s3_bucket.salahdin-ttestt
 }
