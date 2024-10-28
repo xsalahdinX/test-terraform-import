82afd53a06848f70552dc55767d1f19875ceb795
Salahdin_Hafez <102380926+xsalahdinX@users.noreply.github.com>
2024-10-28 14:28:35 +0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "access_key" {
+  description = " from terraform cloud"
+  type        = string
+
+}
+
+variable "secret_key" {
+  description = " from terraform cloud"
+  type        = string
+
+}
+
+variable "region" {
+  description = " from terraform cloud"
+  type        = string
+
+}
