6d2e8d2722098a4e37fba31c0c7ded14a3f500b4
salahdin-hafez <salahdin.hafez@vodafone.com>
2024-11-04 16:33:45 +0200

    add resorces

--- terraform/launch_scaler.tf
+++ terraform/launch_scaler.tf
@@ -94,97 +94,97 @@ resource "aws_security_group" "action_sg" {
 
 # launch template configuration
 
-resource "aws_launch_template" "action_lanch_template" {
-  name          = var.launch_template_name
-  description   = "github-actions-runner-launch-template"
-  image_id      = var.launch_template_ami_id
-  instance_type = var.launch_template_instance_type
-  user_data     = filebase64("./user_data/example.sh")
+# resource "aws_launch_template" "action_lanch_template" {
+#   name          = var.launch_template_name
+#   description   = "github-actions-runner-launch-template"
+#   image_id      = var.launch_template_ami_id
+#   instance_type = var.launch_template_instance_type
+#   user_data     = filebase64("./user_data/example.sh")
 
-  tags = {
-    Confidentiality = "C2"
-  }
+#   tags = {
+#     Confidentiality = "C2"
+#   }
 
-  block_device_mappings {
-    device_name = "/dev/xvda"
+#   block_device_mappings {
+#     device_name = "/dev/xvda"
 
-    ebs {
-      volume_size = 30
-      volume_type = "gp2"
-      delete_on_termination = true
-      encrypted = true
-      kms_key_id = "arn:aws:kms:us-east-1:637423340153:key/a7efc8c0-2981-41ac-87f2-7d7d32286cb7"
-    }
-  }
+#     ebs {
+#       volume_size = 30
+#       volume_type = "gp2"
+#       delete_on_termination = true
+#       encrypted = true
+#       kms_key_id = "arn:aws:kms:us-east-1:637423340153:key/a7efc8c0-2981-41ac-87f2-7d7d32286cb7"
+#     }
+#   }
 
-  hibernation_options {
-    configured = true
-  }
-  iam_instance_profile {
-    arn = aws_iam_instance_profile.action_instance_profile.arn
-  }
-  network_interfaces {
-    security_groups = [aws_security_group.action_sg.id] #p
-    subnet_id       = var.launch_template_subnet_id     #private subnet
+#   hibernation_options {
+#     configured = true
+#   }
+#   iam_instance_profile {
+#     arn = aws_iam_instance_profile.action_instance_profile.arn
+#   }
+#   network_interfaces {
+#     security_groups = [aws_security_group.action_sg.id] #p
+#     subnet_id       = var.launch_template_subnet_id     #private subnet
 
-  }
-  tag_specifications {
-    resource_type = "instance"
-    tags = {
-      Confidentiality   = "C2"
-      Environment       = "DEV"
-      ManagedBy         = "vssefunc.mailboxdeploymentcoe@vodafone.com"
-      ManagementAccount = "242534911695"
-      Name              = "github-actions-resources"
-      Project           = "vois-coe"
-      SecurityZone      = "I-A"
-      TaggingVersion    = "V2.4"
-    }
-  }
-  tag_specifications {
-    resource_type = "volume"
-    tags = {
-      Confidentiality   = "C2"
-      Environment       = "DEV"
-      ManagedBy         = "vssefunc.mailboxdeploymentcoe@vodafone.com"
-      ManagementAccount = "242534911695"
-      Name              = "github-actions-resources"
-      Project           = "vois-coe"
-      SecurityZone      = "I-A"
-      TaggingVersion    = "V2.4"
-    }
-  }
-}
+#   }
+#   tag_specifications {
+#     resource_type = "instance"
+#     tags = {
+#       Confidentiality   = "C2"
+#       Environment       = "DEV"
+#       ManagedBy         = "vssefunc.mailboxdeploymentcoe@vodafone.com"
+#       ManagementAccount = "242534911695"
+#       Name              = "github-actions-resources"
+#       Project           = "vois-coe"
+#       SecurityZone      = "I-A"
+#       TaggingVersion    = "V2.4"
+#     }
+#   }
+#   tag_specifications {
+#     resource_type = "volume"
+#     tags = {
+#       Confidentiality   = "C2"
+#       Environment       = "DEV"
+#       ManagedBy         = "vssefunc.mailboxdeploymentcoe@vodafone.com"
+#       ManagementAccount = "242534911695"
+#       Name              = "github-actions-resources"
+#       Project           = "vois-coe"
+#       SecurityZone      = "I-A"
+#       TaggingVersion    = "V2.4"
+#     }
+#   }
+# }
 
 
-# autoscaling group configuration
+# # autoscaling group configuration
 
-resource "aws_autoscaling_group" "action_asg" {
-  name                      = var.autoscaling_group_name
-  vpc_zone_identifier       = var.autoscaling_group_subnet_ids
-  max_size                  = 2
-  min_size                  = 0
-  desired_capacity          = 0
-  health_check_grace_period = 120
-  health_check_type         = "EC2"
-  default_cooldown          = 1
-  default_instance_warmup   = 0
-  protect_from_scale_in     = true
+# resource "aws_autoscaling_group" "action_asg" {
+#   name                      = var.autoscaling_group_name
+#   vpc_zone_identifier       = var.autoscaling_group_subnet_ids
+#   max_size                  = 2
+#   min_size                  = 0
+#   desired_capacity          = 0
+#   health_check_grace_period = 120
+#   health_check_type         = "EC2"
+#   default_cooldown          = 1
+#   default_instance_warmup   = 0
+#   protect_from_scale_in     = true
 
-  launch_template {
-    id = aws_launch_template.action_lanch_template.id
-  }
-  tag {
-    key                 = "Confidentiality"
-    propagate_at_launch = true
-    value               = "C2"
-  }
-  warm_pool {
-    max_group_prepared_capacity = -1
-    min_size                    = 2
-    pool_state                  = "Stopped"
-    instance_reuse_policy {
-      reuse_on_scale_in = false
-    }
-  }
-}
+#   launch_template {
+#     id = aws_launch_template.action_lanch_template.id
+#   }
+#   tag {
+#     key                 = "Confidentiality"
+#     propagate_at_launch = true
+#     value               = "C2"
+#   }
+#   warm_pool {
+#     max_group_prepared_capacity = -1
+#     min_size                    = 2
+#     pool_state                  = "Stopped"
+#     instance_reuse_policy {
+#       reuse_on_scale_in = false
+#     }
+#   }
+# }
